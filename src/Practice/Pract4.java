fd6b31fc0e1af616487c6d16d883068c5ee92426
seryxme <coolbams24@yahoo.com>
2022-05-25 11:53:34 +0100

    All java works.

---
+++ src/Practice/Pract4.java
@@ -0,0 +1,34 @@
+package Practice;
+
+public class Pract4 {
+    public static void main(String[] args) {
+
+       /* int numberOfStudents = 2;
+        int numberOfSubjects = 4;
+
+        StudentGrade test = new StudentGrade(numberOfStudents, numberOfSubjects);
+
+        test.gradeReport();*/
+
+        int[] array = {76, 96, 85, 63, 45};
+        /*int[] newArray = new int[array.length];
+        int[] position = new int[array.length];
+
+
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        Arrays.sort(newArray);
+        for (int i = 0;i < newArray.length;i++) {
+            position[i] = i + 1;
+        }
+        for (int k : array) {
+            for (int j = 0; j < newArray.length; j++) {
+                if (k == newArray[j]) {
+                    System.out.println(k + " " + position[j]);
+                }
+            }
+        }*/
+
+
+
+    }
+}
